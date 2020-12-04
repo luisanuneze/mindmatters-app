34de0a46f53a59c543c071d122cade0305748fb0
luisf <luisfelipece17@gmail.com>
2020-12-04 08:08:06 -0600

    Unificando el proyecto

--- app/src/main/java/com/example/mindmattersapp/adapters/ForumAdapter.java
+++ app/src/main/java/com/example/mindmattersapp/adapters/ForumAdapter.java
@@ -1,5 +1,7 @@
 package com.example.mindmattersapp.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,10 +9,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mindmattersapp.R;
 import com.example.mindmattersapp.activities.ForumActivity;
+import com.example.mindmattersapp.activities.forum.ForumWebActivity;
 import com.example.mindmattersapp.models.Topics;
 
 import java.util.List;
@@ -33,6 +37,8 @@ public class ForumAdapter extends RecyclerView.Adapter<ForumAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.topicImg.setImageResource(topicsList.get(position).getIdTopicImg());
         holder.topicTitle.setText(topicsList.get(position).getTitulo());
+
+        holder.setOnClickListeners();
     }
 
     @Override
@@ -40,14 +46,29 @@ public class ForumAdapter extends RecyclerView.Adapter<ForumAdapter.ViewHolder> 
         return topicsList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private CardView topicCard;
         private ImageView topicImg;
         private TextView topicTitle;
+        private Context context;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            context = itemView.getContext();
+
             topicImg = (ImageView) itemView.findViewById(R.id.topicImg);
             topicTitle = (TextView) itemView.findViewById(R.id.topicTitle);
+            topicCard = itemView.findViewById(R.id.cardViewPokemonTopic);
+        }
+
+        public void setOnClickListeners() {
+            topicCard.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(context, ForumWebActivity.class);
+            context.startActivity(intent);
         }
     }
 }
