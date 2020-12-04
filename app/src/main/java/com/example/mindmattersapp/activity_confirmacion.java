09087e4132ffa7f0bd2951dfdf3e8343b079dfe9
LuisaNunez1 <luisa.nunez150@gmail.com>
2020-12-04 08:21:52 -0600

    Seguridad Activity

---
+++ app/src/main/java/com/example/mindmattersapp/activity_confirmacion.java
@@ -0,0 +1,14 @@
+package com.example.mindmattersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_confirmacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion);
+    }
+}
